51ff970137f949f36f436585841a2a27b7997517
Nymul Islam Moon <towkirislam@gmail.com>
2023-03-24 12:52:52 +0600

    bootstrap login sysytem done

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'phone',
+        'address',
         'email',
         'password',
     ];
